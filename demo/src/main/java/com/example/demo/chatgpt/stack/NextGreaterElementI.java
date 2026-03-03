36ffdc9f65c9e272a43c4987cf1cbe9a68a0d73c
Sany06 <90442685+Sany06@users.noreply.github.com>
2026-03-03 10:34:45 +0530

    Stack Problems

---
+++ demo/src/main/java/com/example/demo/chatgpt/stack/NextGreaterElementI.java
@@ -0,0 +1,23 @@
+package com.example.demo.chatgpt.stack;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class NextGreaterElementI {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num:nums2){
+            while(!stack.isEmpty() && stack.peek()<num){
+                map.put(stack.pop(),num);
+            }
+            stack.push(num);
+        }
+        int[] ints = new int[nums1.length];
+        for (int i=0;i<nums1.length;i++){
+            ints[i]= map.getOrDefault(nums1[i],-1);
+        }
+        return ints;
+
+    }
+}
