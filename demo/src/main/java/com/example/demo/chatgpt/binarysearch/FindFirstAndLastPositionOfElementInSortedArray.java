1761dacd3478be338c53985363be45960fc2df39
Sany06 <90442685+Sany06@users.noreply.github.com>
2026-03-12 21:40:35 +0530

    BinarySearch problems

---
+++ demo/src/main/java/com/example/demo/chatgpt/binarysearch/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,45 @@
+package com.example.demo.chatgpt.binarysearch;
+
+public class FindFirstAndLastPositionOfElementInSortedArray {
+    public int[] searchRange(int[] nums, int target) {
+        int firstIndex = findFirstIndex(nums, target);
+        int lastIndex = findLastIndex(nums, target);
+        return new int[]{firstIndex, lastIndex};
+    }
+
+    private int findLastIndex(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                ans=mid;
+                start=mid+1;
+            } else if (nums[mid]>target){
+                end=mid-1;
+            } else {
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    private int findFirstIndex(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                ans=mid;
+                end=mid-1;
+            } else if (nums[mid]>target){
+                end=mid-1;
+            } else {
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+}
