d11fae79819830b2a72c21e056b978e6892ef464
Sany06 <90442685+Sany06@users.noreply.github.com>
2026-06-04 11:47:40 +0530

    Sliding window

---
+++ demo/src/main/java/selfpracticeproblems/arrays/slidingwindow/variablewindowsize/LongestSubarrayWithSumKII.java
@@ -0,0 +1,29 @@
+package selfpracticeproblems.arrays.slidingwindow.variablewindowsize;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubarrayWithSumKII {
+    public int longestSubarray(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0, length = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            //Case 1 : if sum == k at i = 1
+            if (sum == k) {
+                length = i + 1;
+            }
+            //Case 2 : check if (sum - k) is seen before
+            if (map.containsKey(sum - k)) {
+                int len = i - map.get(sum - k);
+                length = Math.max(length, len);
+            }
+
+            //Store first occurrence only
+            map.putIfAbsent(sum, i);
+        }
+        return length;
+    }
+}
