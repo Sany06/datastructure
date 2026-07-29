1ce2e8da6c9a7c99b18df11ea0c4fffe6f924e4c
Sany06 <90442685+Sany06@users.noreply.github.com>
2026-07-29 23:48:17 +0530

    DP problems

--- demo/src/main/java/com/example/demo/chatgpt/dynamicprogramming/knapsack/zerooneknapsack/CountOfSubsetSum.java
+++ demo/src/main/java/com/example/demo/chatgpt/dynamicprogramming/knapsack/zerooneknapsack/CountOfSubsetSum.java
@@ -6,26 +6,25 @@ public class CountOfSubsetSum {
         System.out.println(countOfSubsetSum(new int[]{2,3,5,10, 8, 2},10));
     }
 
-    public static int countOfSubsetSum(int[] arr, int sum) {
-        int[][] dp = new int[arr.length + 1][sum + 1];
+    public static int countOfSubsetSum(int[] arr, int target) {
+        int[][] dp = new int[arr.length + 1][target + 1];
 
-        // as here sum is j and sum will always be 0
-        for (int i = 0; i <= arr.length; i++) {
-            dp[i][0] = 1;
-        }
+        dp[0][0] = 1;
 
+//        for (int i = 0; i <= arr.length; i++) {
+//            dp[i][0] = 1;
+//        }
 
-        for (int i = 1; i <= arr.length; i++) {
-            for (int j = 1; j <= sum; j++) {
-
-                if (arr[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j - arr[i - 1]] + dp[i - 1][j];
+        for(int i = 1; i <= arr.length ; i++) {
+            for(int j = 0 ; j <= target ; j++) {
+                if(arr[i - 1] <= j){
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - arr[i - 1]];
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-        return dp[arr.length][sum];
+        return dp[arr.length][target];
     }
 
 }
