4d358fa5a94735711d616bcbe03e149ea1ddeb15
Sany06 <90442685+Sany06@users.noreply.github.com>
2026-08-10 20:02:44 +0530

    DP problems

---
+++ demo/src/main/java/com/example/demo/chatgpt/dynamicprogramming/DecodeWays.java
@@ -0,0 +1,27 @@
+package com.example.demo.chatgpt.dynamicprogramming;
+
+public class DecodeWays {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for(int i = 2; i<=n ; i++) {
+            int onesplace = s.charAt(i - 1) - '0';
+
+            if(onesplace != 0){
+                dp[i] += dp[i - 1];
+            }
+
+            int tensplace = Integer.valueOf(s.substring(i - 2, i));
+
+            if(tensplace >=10 && tensplace<=26){
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
