abed19dd63b7c53b52aead0ea35a941dfec4ea7d
Sany06 <90442685+Sany06@users.noreply.github.com>
2026-09-05 09:48:54 +0530

    DP problems

--- demo/src/main/java/selfpracticeproblems/arrays/greedy/part2/MaximumUnitsOnATruck.java
+++ demo/src/main/java/selfpracticeproblems/arrays/greedy/part2/MaximumUnitsOnATruck.java
@@ -6,13 +6,14 @@ public class MaximumUnitsOnATruck {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
         int totalunits = 0;
 
+        //Sort boxTypes based on the value at index 1, from largest to smallest.
         Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
 
         for(int[] box : boxTypes) {
-            int boxes = Math.min(box[0],truckSize);
+            int numberOfBoxes = Math.min(box[0],truckSize);
 
-            totalunits += boxes * box[1];
-            truckSize -= boxes;
+            totalunits += numberOfBoxes * box[1];
+            truckSize -= numberOfBoxes;
 
             if(truckSize == 0) break;
         }
